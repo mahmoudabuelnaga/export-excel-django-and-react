68df46db18c77524295f593efead8e1a21c39baf
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-07-18 16:56:15 +0200

    export excel from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios'
 import ExportToExcel from './components/Backend';
 import Shrouded from './components/DataShare';
 
 function App() {
-  const [data, setData] = React.useState([])
-  const fileName = "myfile"; // here enter filename for your excel file
-
-  React.useEffect(() => {
-    const fetchData = () =>{
-     axios.get('http://127.0.0.1:8000').then(r => setData(r.data) )
-    }
-    fetchData()
-  }, [])
-
-
   return (
     <div className="App">
       <header className="App-header">
         <Shrouded />
         <hr/>
-        <ExportToExcel apiData={data} fileName={fileName} />
+        <ExportToExcel />
       </header>
-
-      
-      
     </div>
   );
 }
--- frontend/src/components/Backend.js
+++ frontend/src/components/Backend.js
@@ -1,24 +1,9 @@
 import React from 'react'
-import * as FileSaver from "file-saver";
-import * as XLSX from "xlsx";
 
-
-const ExportToExcel = ({ apiData, fileName }) => {
-    const fileType =
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-    const fileExtension = ".xlsx";
-  
-    const exportToCSV = (apiData, fileName) => {
-      const ws = XLSX.utils.json_to_sheet(apiData);
-      const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-      const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-      const data = new Blob([excelBuffer], { type: fileType });
-      FileSaver.saveAs(data, fileName + fileExtension);
-    };
-  
+const ExportToExcel = () => {  
     return (
-      <a href={() => false} className="App-link" onClick={(e) => exportToCSV(apiData, fileName)}>Download Excel File</a>
+      <a href='http://127.0.0.1:8000/export' className="App-link" >Download Excel File</a>
     );
   };
 
-  export default ExportToExcel;
+  export default ExportToExcel; 
